ba55220b2df8982fe15faed511474dfdebac7977
RedInquisitive <aj.walter@aol.com>
2015-09-20 00:30:16 -0500

    Edited the folder name, caused all files to change.

---
+++ Botnet Heroes/src/game/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains the class needed to run the base game of Botnet Heroes.
+ *
+ * @author AJ
+ */
+package game;
--- Botnet Heros/src/game/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package contains the class needed to run the base game of Botnet Heros.
- *
- * @author AJ
- */
-package game;
